aaebeffc19aebf89948913878862abb55a0a8802
Juan Sanchez <58664932+juanamedh@users.noreply.github.com>
2022-08-26 11:39:47 -0700

    fixed data type and added js selectors

---
+++ script.js
@@ -0,0 +1,7 @@
+const h1 = document.querySelector('h1');
+const p = document.querySelector('p');
+const paragraph = document.querySelector('.paragraph');
+const pid = document.querySelector('#pid');
+const input = document.querySelector('input');
+
+console.log(input.value)
