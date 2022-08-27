b6867c3567972d3877489ceb8bf2c4c5ceb902cd
Juan Sanchez <58664932+juanamedh@users.noreply.github.com>
2022-08-27 11:52:22 -0700

    fixed  typo

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const arg2 = document.querySelector('#arg2');
 const btn = document.querySelector('#calcBttn');
 const pTotal = document.querySelector('#total')
 
+btn.addEventListener('click', bttnOnclick);
+
 function bttnOnclick() {
     const addition = parseInt(arg1.value) + parseInt(arg2.value);
     pTotal.innerText = "Your total is " + addition;
