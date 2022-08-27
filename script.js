f4d6bb349d1d56cd7ef8d996a94d0a3cc575c78b
Juan Sanchez <58664932+juanamedh@users.noreply.github.com>
2022-08-27 12:27:09 -0700

    Formatted my inputs in a usable form

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const h1 = document.querySelector('h1');
+const form = document.querySelector('#form');
 const arg1 = document.querySelector('#arg1');
 const arg2 = document.querySelector('#arg2');
 const btn = document.querySelector('#calcBttn');
-const pTotal = document.querySelector('#total')
+const pTotal = document.querySelector('#total');
 
-btn.addEventListener('click', bttnOnclick);
+form.addEventListener('submit', bttnOnclick);
 
-function bttnOnclick() {
+function bttnOnclick(event) {
+    event.preventDefault()
     const addition = parseInt(arg1.value) + parseInt(arg2.value);
     pTotal.innerText = "Your total is " + addition;
 }
