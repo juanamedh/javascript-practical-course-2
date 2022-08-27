492b6913cd3a6d19fcd8a19014f320f4a570ec33
Juan Sanchez <58664932+juanamedh@users.noreply.github.com>
2022-08-27 11:45:32 -0700

    Now I can make additions and show them in the DOM

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const h1 = document.querySelector('h1');
-const arg1 = document.querySelector('#arg1')
-const arg2 = document.querySelector('#arg2')
+const arg1 = document.querySelector('#arg1');
+const arg2 = document.querySelector('#arg2');
+const btn = document.querySelector('#calcBttn');
+const pTotal = document.querySelector('#total')
+
+function bttnOnclick() {
+    const addition = parseInt(arg1.value) + parseInt(arg2.value);
+    pTotal.innerText = "Your total is " + addition;
+}
